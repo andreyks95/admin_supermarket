5bfffff5701f161ffb3ab68f62b9eb9dec7c7448
andreyks95 <andreyks95@gmail.com>
2017-01-03 23:25:50 +0200

    Add ComboBox

--- Administrator_company/Administrator_company/Preview (Test)/TestForm.cs
+++ Administrator_company/Administrator_company/Preview (Test)/TestForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Administrator_company.Preview__Test_
 {
@@ -15,6 +16,26 @@ namespace Administrator_company.Preview__Test_
         public TestForm()
         {
             InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlConnection connection = new MySqlConnection("datasource=localhost; port=3306; username = root; password = andrey_1a6c2b");
+                string selectQuery = "SELECT * FROM supermarket.products";
+                connection.Open();
+                MySqlCommand command = new MySqlCommand(selectQuery,connection);
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    comboBox1.Items.Add(reader.GetValue(1));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
