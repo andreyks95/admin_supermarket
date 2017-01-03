9c86fc12ef689db4730f3b6f830329e046ae4d3d
andreyks95 <andreyks95@gmail.com>
2017-01-03 21:43:12 +0200

    Change design and view data on table

--- Administrator_company/Administrator_company/Preview (Test)/Stock.Designer.cs
+++ Administrator_company/Administrator_company/Preview (Test)/Stock.Designer.cs
@@ -35,10 +35,14 @@
             // 
             // dataGridView1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.DefaultCellStyle.NullValue = null;
             this.dataGridView1.Size = new System.Drawing.Size(948, 463);
             this.dataGridView1.TabIndex = 0;
             // 
@@ -59,6 +63,7 @@
             this.Controls.Add(this.dataGridView1);
             this.Name = "Stock";
             this.Text = "Stock";
+            this.Load += new System.EventHandler(this.Stock_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
--- Administrator_company/Administrator_company/Preview (Test)/Stock.cs
+++ Administrator_company/Administrator_company/Preview (Test)/Stock.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Administrator_supermarket;
 
 namespace Administrator_company.Preview__Test_
 {
@@ -15,6 +16,16 @@ namespace Administrator_company.Preview__Test_
         public Stock()
         {
             InitializeComponent();
+        }
+        Connection connection = new Connection();//для отображения, записи, изменения, удаления данных в таблице
+        Queries queries = new Queries();//для запросов выборки
+        private void Stock_Load(object sender, EventArgs e)
+        {
+
+            string query = "select products.name, stock.available, stock.entered, stock.sold, stock.quantity, products.measurement, stock.price "+
+                           " From supermarket.products, supermarket.stock " +
+                           " where supermarket.products.id_products = supermarket.stock.id_products; ";
+            connection.ShowTable(dataGridView1, query);
         }
     }
 }
