f6ac7f649d49584c87394562222cc5a3b6f1b114
andreyks95 <andreyks95@gmail.com>
2017-01-04 22:43:07 +0200

    Add comments and new event dataGridView1_SelectionChanged

--- Administrator_company/Administrator_company/Preview (Test)/TestFormTwo.cs
+++ Administrator_company/Administrator_company/Preview (Test)/TestFormTwo.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,11 +21,14 @@ namespace Administrator_company.Preview__Test_
             InitializeComponent();
         }
         public MySqlConnection connection = new MySqlConnection("datasource=localhost; port=3306; username = root; password = andrey_1a6c2b");
+        
+        //При загрузке отобразить таблицу
         private void TestFormTwo_Load(object sender, EventArgs e)
         {
             FillDataGridView();
         }
 
+        //Заполняем DataGridView и корректируем её
         public void FillDataGridView()
         {
             MySqlCommand command = new MySqlCommand("SELECT * FROM supermarket.info", connection); //Создаём запрос для выполнения команды
@@ -60,6 +62,7 @@ namespace Administrator_company.Preview__Test_
 
         }
 
+        //для выбора изображения 
         private void BTN_CHOOSE_IMAGE_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFile = new OpenFileDialog();
@@ -71,23 +74,37 @@ namespace Administrator_company.Preview__Test_
             }
         }
 
+        //Когда пользователь будет кликать мышью по нужной строке, то в textBox-ах и pictuerBox будут отображаться данные текущей строки
         private void dataGridView1_Click(object sender, EventArgs e)
         {
-
-           // Byte[] img = (Byte[])dataGridView1.CurrentRow.Cells[6].Value;
-           // MemoryStream ms = new MemoryStream(img);
-           // pictureBox1.Image = Image.FromStream(ms);
-           //или можно так:
-
+            //отображаем текущую картинку из DataGridView в pictureBox
             CurrentRowCellsIMG(6, pictureBox1, dataGridView1);
-            //CurrentRowCellsTEXT(0, textBox1, dataGridView1); 
+            //Массив куда будут (в textBox-ы) отображаться значения текущей строки из DataGridView
             TextBox[] textBoxs = {textBox1, textBox2, textBox3, textBox4, textBox5, textBox6};
+            //отображаем все текстовые записи из DataGridView в textBox-ы
             CurrentRowCellsTEXT(textBoxs, dataGridView1);
         }
 
+        //Когда пользователь будет нажимать стрелками клавиш "вверх" и "вниз" то будут в textBox-ах и pictuerBox отображаться данные текущей строки
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            //отображаем текущую картинку из DataGridView в pictureBox
+            CurrentRowCellsIMG(6, pictureBox1, dataGridView1);
+            //Массив куда будут (в textBox-ы) отображаться значения текущей строки из DataGridView
+            TextBox[] textBoxs = { textBox1, textBox2, textBox3, textBox4, textBox5, textBox6 };
+            //отображаем все текстовые записи из DataGridView в textBox-ы
+            CurrentRowCellsTEXT(textBoxs, dataGridView1);
+        }
 
+        /// <summary>
+        /// Отображает из текущей ячейки строи DataGridView картинку в PicureBox
+        /// </summary>
+        /// <param name="number">номер столбца (ячейки) DataGridView в котором содержиться картинка</param>
+        /// <param name="pictureBox">pictureBox куда нужно вставить картинку</param>
+        /// <param name="dataGridView">текущий dataGridView</param>
         public void CurrentRowCellsIMG(int number, PictureBox pictureBox, DataGridView dataGridView)
         {
+            //Если ячейка пустая, то в pictureBox ничего не отображать
             if (dataGridView.CurrentRow.Cells[number].Value  == DBNull.Value)
             {
                 pictureBox.Image = null;
@@ -96,9 +113,9 @@ namespace Administrator_company.Preview__Test_
             {
                 try
                 {
-                    Byte[] img = (Byte[]) dataGridView.CurrentRow.Cells[number].Value;
+                    Byte[] img = (Byte[]) dataGridView.CurrentRow.Cells[number].Value; //Получаем изображание
                     MemoryStream ms = new MemoryStream(img);
-                    pictureBox.Image = Image.FromStream(ms);
+                    pictureBox.Image = Image.FromStream(ms); // вставляем в pictureBox это изображение
                 }
                 catch (Exception ex)
                 {
@@ -107,25 +124,30 @@ namespace Administrator_company.Preview__Test_
                         
             }
         }
-
+        /// <summary>
+        /// Помещает в textBox значение с ячейки текущей строки DataGridView
+        /// </summary>
+        /// <param name="number">Номер столбца (ячейки) DataGridView в которой содержиться текстовое значение</param>
+        /// <param name="textBox">textBox куда нужно вставить значение</param>
+        /// <param name="dataGridView">текущий dataGridView</param>
         public void CurrentRowCellsTEXT(int number, TextBox textBox, DataGridView dataGridView)
         {
             textBox.Text = dataGridView.CurrentRow.Cells[number].Value.ToString();
         }
-
+        /// <summary>
+        /// Помещает во ВСЕ textBox-ы значения из текущей строки DataGridView
+        /// </summary>
+        /// <param name="textBoxs">Массив textBox-ов куда нужно вставить значения</param>
+        /// <param name="dataGridView">текущий dataGridView</param>
         public void CurrentRowCellsTEXT(TextBox[] textBoxs, DataGridView dataGridView)
         {
+
             for (var i=0; i< textBoxs.Length; i++)
             {
-                CurrentRowCellsTEXT(i, textBoxs[i], dataGridView);
+                //передаёт номер ячейки и сам textBox куда нужно вставить значения из строки DataGridView
+                CurrentRowCellsTEXT(i, textBoxs[i], dataGridView); 
             }
         }
 
-        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
-        {
-            CurrentRowCellsIMG(6, pictureBox1, dataGridView1);
-            TextBox[] textBoxs = { textBox1, textBox2, textBox3, textBox4, textBox5, textBox6 };
-            CurrentRowCellsTEXT(textBoxs, dataGridView1);
-        }
     }
 }
