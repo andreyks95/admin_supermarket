9d45d4eaa6b1e8f60f4194e414edaca7507e045d
andreyks95 <andreyks95@gmail.com>
2017-01-05 22:07:26 +0200

    Add new function for Buttons v 1.0.10.18

--- Administrator_company/Administrator_company/Preview (Test)/TestForm.cs
+++ Administrator_company/Administrator_company/Preview (Test)/TestForm.cs
@@ -14,6 +14,8 @@ namespace Administrator_company.Preview__Test_
 {
     public partial class TestForm : Form
     {
+        BindingManagerBase managerBase;
+
         public TestForm()
         {
             InitializeComponent();
@@ -32,17 +34,60 @@ namespace Administrator_company.Preview__Test_
                 //MySqlConnection connection = new MySqlConnection("datasource=localhost; port=3306; username = root; password = andrey_1a6c2b");
                 string selectQuery = "SELECT * FROM supermarket.products";
                 connect.connection.Open();
+                //для comboBox-а и для DataGridView
                 connect.command = new MySqlCommand(selectQuery, connect.connection);
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter(connect.command);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                dataGridView1.DataSource = table;
+
+                textBox1.DataBindings.Add("text", table, "id_products");
+                textBox2.DataBindings.Add("text", table, "name");
+                textBox3.DataBindings.Add("text", table, "category");
+                textBox4.DataBindings.Add("text", table, "kind");
+                textBox5.DataBindings.Add("text", table, "subspecies");
+                textBox6.DataBindings.Add("text", table, "price_for_one");
+                textBox7.DataBindings.Add("text", table, "measurement");
+
+                
+                managerBase = this.BindingContext[table];
+
+                //для comboBox-а
                 MySqlDataReader reader = connect.command.ExecuteReader();
                 while (reader.Read())
                 {
                     comboBox1.Items.Add(reader.GetValue(1)); //Получаем все значения products.name
                 }
+                
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
         }
+
+        //Начало
+        private void button1_Click(object sender, EventArgs e)
+        {
+            managerBase.Position = 0;
+        }
+        //Следующий
+        private void button2_Click(object sender, EventArgs e)
+        {
+            managerBase.Position += 1;
+        }
+        //Предыдущий
+        private void button3_Click(object sender, EventArgs e)
+        {
+            managerBase.Position -= 1;
+        }
+        //Конец
+        private void button4_Click(object sender, EventArgs e)
+        {
+            managerBase.Position = managerBase.Count;
+        }
+
+
     }
 }
