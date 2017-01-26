efadb4a287c6ad4aa9c945be30b6d9c76c97c035
andreyks95 <andreyks95@gmail.com>
2017-01-26 22:08:56 +0200

    Add comments End changed FORM! v. 1.1.16.61

--- Administrator_company/Administrator_company/Preview (Test)/TestFormTwo.cs
+++ Administrator_company/Administrator_company/Preview (Test)/TestFormTwo.cs
@@ -15,10 +15,10 @@ using Administrator_supermarket;
 
 namespace Administrator_company.Preview__Test_
 {
-    public partial class TestFormTwo : Form
+    public partial class InfoForm : Form
     {
 
-        public TestFormTwo()
+        public InfoForm()
         {
             InitializeComponent();
         }
@@ -26,23 +26,23 @@ namespace Administrator_company.Preview__Test_
         Connection connection = new Connection();
         Settings settings = new Settings();
         Checking checking = new Checking();
-        BindingManagerBase managerBase;
+        BindingManagerBase managerBase; //для перемещения по таблице 
 
 
-        private static string nameTable = "info",
-                              id_field = "id_info";
+        private static string nameTable = "info", //таблица
+                              id_field = "id_info"; //id поле таблицы
         private static string[] nameFieldsAll = { "id_info", "full_name", "passport_id", "age", "address", "phone", "photo" }, //все поля
                                  variables = { "@id", "@name", "@passport", "@age", "@address", "@phone", "@photo" }, //для переменных
                                  nameFieldsAS = { "ИД", "ФИО", "Серия и номер паспорта", "Возраст", "Адрес", "Телефон", "Фото" }, //как будут отображаться
                                  numericFields = { "id_info", "age" }; //для корректного поиска по числовым столбцам                                                    
         private static MySqlDbType[] mySqlDbTypes = { MySqlDbType.UInt32, MySqlDbType.VarChar, MySqlDbType.VarChar, MySqlDbType.UInt32,
-                                           MySqlDbType.MediumText, MySqlDbType.VarChar, MySqlDbType.LongBlob };//для типов
+                                           MySqlDbType.MediumText, MySqlDbType.VarChar, MySqlDbType.LongBlob };//для типов в (AddParameters)
 
 
         //При загрузке отобразить таблицу
         private void TestFormTwo_Load(object sender, EventArgs e)
         {
-            FillDataGridView("");           
+            FillDataGridView(""); //при загрузке отображаем таблицу          
         }
 
 
@@ -58,14 +58,14 @@ namespace Administrator_company.Preview__Test_
             //Для отображения картинки в DataGridView
             //settings.GetViewImageInCellTable(dataGridView1, 6);
             //((DataGridViewImageColumn)this.dataGridView1.Columns["info.photo"]).DefaultCellStyle.NullValue = null;
-            DataTable table = connection.FillDataGridView(dataGridView1, 100, new[] {6}, query);
-            managerBase = this.BindingContext[table];
+            DataTable table = connection.FillDataGridView(dataGridView1, 100, new[] {6}, query); //заполняем таблицу данными с запроса и настраиваем
+            managerBase = this.BindingContext[table]; //подключаем таблицу для передвижения по ней
         }
 
         //для выбора изображения 
         private void BTN_CHOOSE_IMAGE_Click(object sender, EventArgs e)
         {
-            settings.GetChooseImage(pictureBox1);
+            settings.GetChooseImage(pictureBox1); //выбрать изображение из файла
         }
 
 
@@ -73,54 +73,55 @@ namespace Administrator_company.Preview__Test_
         private void dataGridView1_Click(object sender, EventArgs e)
         {
             //отображаем текущую картинку из DataGridView в pictureBox
-            settings.CurrentRowCellsIMG(6, pictureBox1, dataGridView1);
+            settings.CurrentColumnCellsIMG(6, pictureBox1, dataGridView1);
 
             //Массив куда будут (в textBox-ы) отображаться значения текущей строки из DataGridView
             TextBox[] textBoxs = { textBox1, textBox2, textBox3, textBox4, textBox5, textBox6 };
             //отображаем все текстовые записи из DataGridView в textBox-ы
-            settings.CurrentRowCellsTEXT(textBoxs, dataGridView1);
+            settings.CurrentColumnCellsTEXT(textBoxs, dataGridView1);
         }
 
         //Когда пользователь будет нажимать стрелками клавиш "вверх" и "вниз" то будут в textBox-ах и pictuerBox отображаться данные текущей строки
         private void dataGridView1_SelectionChanged(object sender, EventArgs e)
         {
             //отображаем текущую картинку из DataGridView в pictureBox
-            settings.CurrentRowCellsIMG(6, pictureBox1, dataGridView1);
-
+            settings.CurrentColumnCellsIMG(6, pictureBox1, dataGridView1);
             //Массив куда будут (в textBox-ы) отображаться значения текущей строки из DataGridView
             TextBox[] textBoxs = { textBox1, textBox2, textBox3, textBox4, textBox5, textBox6 };
             //отображаем все текстовые записи из DataGridView в textBox-ы
-            settings.CurrentRowCellsTEXT(textBoxs, dataGridView1);
+            settings.CurrentColumnCellsTEXT(textBoxs, dataGridView1);
         }
 
+        //Вставить данные 
         private void Insert_Click(object sender, EventArgs e)
         {
+            //Если есть изображение, тогда добавляем
             if (pictureBox1.Image != null)
             {
                 //возвращаем результаты проверок всех полей
                 bool resultSecurity = checking.SecurityAll(textBox1, textBox2, textBox3, textBox5, textBox6),
                     resultVoid = checking.VoidAll(textBox1, textBox2, textBox3); //Проверяем только обязательные для ввода поля
-                                                                                 //если результаты вернулись положительные, тогда можно добавить данные, иначе вывести ошибку
+                //если результаты вернулись положительные, тогда можно добавить данные, иначе вывести ошибку
                 if (resultSecurity == true && resultVoid == true)
                 {
-                    //1-й этап
+                    //Сохранить изображение из pictureBox в массив byte[]
                     byte[] img = settings.SaveImagesToBytes(pictureBox1);
-
+                    //получить запрос для вставки Insert
                     string query = connection.GetQueryInsert(nameTable, nameFieldsAll, variables);
-
-                    //3-й этап
+                    //выполняить команду с Insert
                     connection.command = new MySqlCommand(query, connection.connection);
-
-                    //для объектов
+                    //для объектов, у них есть данные которые нужно вставить
                     object[] objects = {textBox1, textBox2, textBox3, textBox4, textBox5, textBox6, img};
+                    //Добавляем данные 
                     connection.AddParameters(connection.command, variables, mySqlDbTypes, objects);
-
+                    //попытаться выполнить запрос
                     connection.ExecuteQuery(connection.command);
+                    //отобразить новые данные 
                     FillDataGridView("");
                 }
                 else
                 {
-                    checking.ErrorMessage(this);
+                    checking.ErrorMessage(this); //вывести ошибку
                 }
             }
             else
@@ -129,25 +130,28 @@ namespace Administrator_company.Preview__Test_
             }
         }
 
+        //Обновленяем нужные нам данные
         private void Update_Click(object sender, EventArgs e)
         {
             //возвращаем результаты проверок всех полей
             bool resultSecurity = checking.SecurityAll(textBox1, textBox2, textBox3, textBox5, textBox6),
                 resultVoid = checking.VoidAll(textBox1, textBox2, textBox3); //Проверяем только обязательные для ввода поля
-                                                                                                           //если результаты вернулись положительные, тогда можно добавить данные, иначе вывести ошибку
+           //если результаты вернулись положительные, тогда можно добавить данные, иначе вывести ошибку
             if (resultSecurity == true && resultVoid == true)
             {
+                //Сохранить изображение из pictureBox в массив byte[]
                 byte[] img = settings.SaveImagesToBytes(pictureBox1);
-
+                //Получаем запрос для обновления Update
                 string query = connection.GetQueryUpdate(nameTable, nameFieldsAll, variables, id_field);
-
+                //выполнить команду
                 connection.command = new MySqlCommand(query, connection.connection);
-
-                //для объектов
+                //для объектов, у них есть данные которые нужно вставить
                 object[] objects = {textBox1, textBox2, textBox3, textBox4, textBox5, textBox6, img};
+                //Добавляем данные 
                 connection.AddParameters(connection.command, variables, mySqlDbTypes, objects);
-
+                //попытаться выполнить запрос
                 connection.ExecuteQuery(connection.command);
+                //отобразить новые данные 
                 FillDataGridView("");
             }
             else
@@ -156,26 +160,31 @@ namespace Administrator_company.Preview__Test_
             }
         }
 
+        //удаляем данные
         private void Delete_Click(object sender, EventArgs e)
         {
+            //возвращаем результаты проверок всех полей
             bool resultSecurity = checking.SecurityAll(textBox1),
-                resultVoid = checking.VoidAll(textBox1);
+                resultVoid = checking.VoidAll(textBox1);//Проверяем только обязательные для ввода поля
+            //если результаты вернулись положительные, тогда можно добавить данные, иначе вывести ошибку
             if (resultSecurity == true && resultVoid == true)
             {
+                //Получаем запрос для удаления данных Delete
                 string query = connection.GetQueryDelete(nameTable, id_field);
-
+                //выполняем запрос
                 connection.command = new MySqlCommand(query, connection.connection);
-
-                //для объектов
+                //для объектов, у них есть данные которые нужно вставить
                 object[] objects = { textBox1, textBox2, textBox3, textBox4, textBox5, textBox6 };
-
+                // Добавляем данные
                 connection.AddParameters(connection.command, variables[0], mySqlDbTypes[0], objects[0]);
-
+                //попытаться выполнить запрос
                 connection.ExecuteQuery(connection.command);
+                //отобразить новые данные 
                 FillDataGridView("");
-
+                //Что есть на форме
                 TextBox[] textBoxs = { textBox1, textBox2, textBox3, textBox4, textBox5, textBox6 };
                 PictureBox[] pictureBoxs = { pictureBox1 };
+                //Очищяем поля
                 settings.ClearFields(textBoxs, pictureBoxs: pictureBoxs);
             }
             else
@@ -185,27 +194,31 @@ namespace Administrator_company.Preview__Test_
 
         }
 
+        //Нахождение записи по id
         private void Find_Click(object sender, EventArgs e)
         {
             bool resultSecurity = checking.SecurityAll(textBox1),
                     resultVoid = checking.VoidAll(textBox1);
             if (resultSecurity == true && resultVoid == true)
             {
+                //Как будут подписаны поля 
                     string[] nameFieldsAS = { "ИД", "ФИО", "'Серия и номер паспорта'", "Возраст", "Адрес", "Телефон", "Фото" };
-
+                //получаем запрос для нахождения искомого значения
                 string query = connection.GetQueryFindSelect(nameTable, nameFieldsAll, nameFieldsAS, id_field);
-
+                //выполнить запрос
                 connection.command = new MySqlCommand(query, connection.connection); //Создаём запрос для поиска
-                //для объектов
-                object[] objects = { textBox1, textBox2, textBox3, textBox4, textBox5, textBox6 };
+                //для объектов, у них есть данные которые нужно вставить
+                object[] objects = { textBox1};
 
-                connection.AddParameters(connection.command, variables[0], mySqlDbTypes[0], objects[0]);
+                connection.AddParameters(connection.command, variables[0], mySqlDbTypes[0], objects);
 
                 TextBox[] textBoxs = { textBox1, textBox2, textBox3, textBox4, textBox5, textBox6 };
                 PictureBox[] pictureBoxs = { pictureBox1 };
-                int[] rowsTextForTextBox = { 0, 1, 2, 3, 4, 5 };
-                DataTable table = connection.Find(connection.command, textBoxs: textBoxs, rowsTextForTextBox: rowsTextForTextBox,
-                                                    pictureBoxs: pictureBoxs, rowsPictureForPictureBox: new[] { 6 });
+                int[] ColumnsTextForTextBox = { 0, 1, 2, 3, 4, 5 };//столбцы таблицы с которых нужно взять данные и вставить в TextBox-ы
+                //Найти запись 
+                DataTable table = connection.Find(connection.command, textBoxs: textBoxs, ColumnsTextForTextBox: ColumnsTextForTextBox,
+                                                    pictureBoxs: pictureBoxs, ColumnsPictureForPictureBox: new[] { 6 }, dataGrid:dataGridView1);
+                //для передвижения по таблице
                 managerBase = this.BindingContext[table];
             }
             else
@@ -214,17 +227,20 @@ namespace Administrator_company.Preview__Test_
             }
         }
 
+        //Для поиска
         private void textBoxSearch_TextChanged(object sender, EventArgs e)
         {
             FillDataGridView(textBoxSearch.Text);
         }
 
+        //Очисть поля
         private void Clear_Click(object sender, EventArgs e)
         {
             TextBox[] textBoxs = { textBox1, textBox2, textBox3, textBox4, textBox5, textBox6 };
             PictureBox[] pictureBoxs = { pictureBox1 };
             settings.ClearFields(textBoxs, pictureBoxs: pictureBoxs);
         }
+
 
         private void FirstRecordButton_Click(object sender, EventArgs e)
         {
