00c56bdcf9b80ce2d0cb91adbdd71b2a826b9e55
andreyks95 <andreyks95@gmail.com>
2017-03-03 00:15:43 +0200

    Changed button Add top menu

--- Administrator_company/Administrator_company/Preview (Test)/MainForm.Designer.cs
+++ Administrator_company/Administrator_company/Preview (Test)/MainForm.Designer.cs
@@ -37,33 +37,45 @@
             this.ProductsButton = new System.Windows.Forms.Button();
             this.stock = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.таблицыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сервисToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.информацияОСотрудникахToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.должностиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сотрудникиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.продуктыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.складToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // OpenStock
             // 
-            this.OpenStock.Location = new System.Drawing.Point(117, 12);
+            this.OpenStock.Location = new System.Drawing.Point(12, 257);
             this.OpenStock.Name = "OpenStock";
             this.OpenStock.Size = new System.Drawing.Size(96, 23);
             this.OpenStock.TabIndex = 0;
             this.OpenStock.Text = "Склад";
             this.OpenStock.UseVisualStyleBackColor = true;
+            this.OpenStock.Visible = false;
             this.OpenStock.Click += new System.EventHandler(this.OpenStock_Click);
             // 
             // OpenTestForm
             // 
-            this.OpenTestForm.Location = new System.Drawing.Point(117, 53);
+            this.OpenTestForm.Location = new System.Drawing.Point(114, 257);
             this.OpenTestForm.Name = "OpenTestForm";
             this.OpenTestForm.Size = new System.Drawing.Size(96, 23);
             this.OpenTestForm.TabIndex = 1;
             this.OpenTestForm.Text = "Test Form";
             this.OpenTestForm.UseVisualStyleBackColor = true;
+            this.OpenTestForm.Visible = false;
             this.OpenTestForm.Click += new System.EventHandler(this.OpenTestForm_Click);
             // 
             // TestFormTwo
             // 
-            this.TestFormTwo.Location = new System.Drawing.Point(12, 13);
+            this.TestFormTwo.Location = new System.Drawing.Point(55, 27);
             this.TestFormTwo.Name = "TestFormTwo";
-            this.TestFormTwo.Size = new System.Drawing.Size(96, 38);
+            this.TestFormTwo.Size = new System.Drawing.Size(140, 40);
             this.TestFormTwo.TabIndex = 2;
             this.TestFormTwo.Text = "Информация о сотрудниках";
             this.TestFormTwo.UseVisualStyleBackColor = true;
@@ -71,9 +83,9 @@
             // 
             // PositionButton
             // 
-            this.PositionButton.Location = new System.Drawing.Point(12, 70);
+            this.PositionButton.Location = new System.Drawing.Point(55, 73);
             this.PositionButton.Name = "PositionButton";
-            this.PositionButton.Size = new System.Drawing.Size(96, 23);
+            this.PositionButton.Size = new System.Drawing.Size(140, 40);
             this.PositionButton.TabIndex = 3;
             this.PositionButton.Text = "Должности";
             this.PositionButton.UseVisualStyleBackColor = true;
@@ -81,9 +93,9 @@
             // 
             // EmployeesButton
             // 
-            this.EmployeesButton.Location = new System.Drawing.Point(12, 109);
+            this.EmployeesButton.Location = new System.Drawing.Point(55, 119);
             this.EmployeesButton.Name = "EmployeesButton";
-            this.EmployeesButton.Size = new System.Drawing.Size(96, 23);
+            this.EmployeesButton.Size = new System.Drawing.Size(140, 40);
             this.EmployeesButton.TabIndex = 4;
             this.EmployeesButton.Text = "Сотрудники";
             this.EmployeesButton.UseVisualStyleBackColor = true;
@@ -92,28 +104,29 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(114, 109);
+            this.label1.Location = new System.Drawing.Point(9, 119);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.Size = new System.Drawing.Size(43, 26);
             this.label1.TabIndex = 5;
-            this.label1.Text = "подчинённая";
+            this.label1.Text = "подчи-\r\nнённая";
+            this.label1.Visible = false;
             // 
             // ProductsButton
             // 
-            this.ProductsButton.Location = new System.Drawing.Point(12, 147);
+            this.ProductsButton.Location = new System.Drawing.Point(55, 165);
             this.ProductsButton.Name = "ProductsButton";
-            this.ProductsButton.Size = new System.Drawing.Size(96, 23);
-            this.ProductsButton.TabIndex = 6;
+            this.ProductsButton.Size = new System.Drawing.Size(140, 40);
+            this.ProductsButton.TabIndex = 5;
             this.ProductsButton.Text = "Продукты";
             this.ProductsButton.UseVisualStyleBackColor = true;
             this.ProductsButton.Click += new System.EventHandler(this.ProductsButton_Click);
             // 
             // stock
             // 
-            this.stock.Location = new System.Drawing.Point(12, 185);
+            this.stock.Location = new System.Drawing.Point(55, 211);
             this.stock.Name = "stock";
-            this.stock.Size = new System.Drawing.Size(96, 23);
-            this.stock.TabIndex = 7;
+            this.stock.Size = new System.Drawing.Size(140, 40);
+            this.stock.TabIndex = 6;
             this.stock.Text = "Склад";
             this.stock.UseVisualStyleBackColor = true;
             this.stock.Click += new System.EventHandler(this.stock_Click);
@@ -121,17 +134,91 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(114, 190);
+            this.label2.Location = new System.Drawing.Point(9, 211);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.Size = new System.Drawing.Size(43, 26);
             this.label2.TabIndex = 8;
-            this.label2.Text = "подчинённая";
+            this.label2.Text = "подчи-\r\nнённая";
+            this.label2.Visible = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.таблицыToolStripMenuItem,
+            this.сервисToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(234, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // таблицыToolStripMenuItem
+            // 
+            this.таблицыToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.информацияОСотрудникахToolStripMenuItem,
+            this.должностиToolStripMenuItem,
+            this.сотрудникиToolStripMenuItem,
+            this.продуктыToolStripMenuItem,
+            this.складToolStripMenuItem});
+            this.таблицыToolStripMenuItem.Name = "таблицыToolStripMenuItem";
+            this.таблицыToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.таблицыToolStripMenuItem.Text = "Таблицы";
+            // 
+            // сервисToolStripMenuItem
+            // 
+            this.сервисToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.оПрограммеToolStripMenuItem});
+            this.сервисToolStripMenuItem.Name = "сервисToolStripMenuItem";
+            this.сервисToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.сервисToolStripMenuItem.Text = "Сервис";
+            // 
+            // информацияОСотрудникахToolStripMenuItem
+            // 
+            this.информацияОСотрудникахToolStripMenuItem.Name = "информацияОСотрудникахToolStripMenuItem";
+            this.информацияОСотрудникахToolStripMenuItem.Size = new System.Drawing.Size(229, 22);
+            this.информацияОСотрудникахToolStripMenuItem.Text = "Информация о сотрудниках";
+            this.информацияОСотрудникахToolStripMenuItem.Click += new System.EventHandler(this.информацияОСотрудникахToolStripMenuItem_Click);
+            // 
+            // должностиToolStripMenuItem
+            // 
+            this.должностиToolStripMenuItem.Name = "должностиToolStripMenuItem";
+            this.должностиToolStripMenuItem.Size = new System.Drawing.Size(229, 22);
+            this.должностиToolStripMenuItem.Text = "Должности";
+            this.должностиToolStripMenuItem.Click += new System.EventHandler(this.должностиToolStripMenuItem_Click);
+            // 
+            // сотрудникиToolStripMenuItem
+            // 
+            this.сотрудникиToolStripMenuItem.Name = "сотрудникиToolStripMenuItem";
+            this.сотрудникиToolStripMenuItem.Size = new System.Drawing.Size(229, 22);
+            this.сотрудникиToolStripMenuItem.Text = "Сотрудники";
+            this.сотрудникиToolStripMenuItem.Click += new System.EventHandler(this.сотрудникиToolStripMenuItem_Click);
+            // 
+            // продуктыToolStripMenuItem
+            // 
+            this.продуктыToolStripMenuItem.Name = "продуктыToolStripMenuItem";
+            this.продуктыToolStripMenuItem.Size = new System.Drawing.Size(229, 22);
+            this.продуктыToolStripMenuItem.Text = "Продукты";
+            this.продуктыToolStripMenuItem.Click += new System.EventHandler(this.продуктыToolStripMenuItem_Click);
+            // 
+            // складToolStripMenuItem
+            // 
+            this.складToolStripMenuItem.Name = "складToolStripMenuItem";
+            this.складToolStripMenuItem.Size = new System.Drawing.Size(229, 22);
+            this.складToolStripMenuItem.Text = "Склад";
+            this.складToolStripMenuItem.Click += new System.EventHandler(this.складToolStripMenuItem_Click);
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(234, 231);
+            this.ClientSize = new System.Drawing.Size(234, 291);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.stock);
             this.Controls.Add(this.ProductsButton);
@@ -141,9 +228,15 @@
             this.Controls.Add(this.TestFormTwo);
             this.Controls.Add(this.OpenTestForm);
             this.Controls.Add(this.OpenStock);
-            this.MinimumSize = new System.Drawing.Size(250, 270);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(250, 330);
             this.Name = "MainForm";
-            this.Text = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "Супермаркет";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -160,5 +253,14 @@
         private System.Windows.Forms.Button ProductsButton;
         private System.Windows.Forms.Button stock;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem таблицыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сервисToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem информацияОСотрудникахToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem должностиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сотрудникиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem продуктыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem складToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
     }
 }
--- Administrator_company/Administrator_company/Preview (Test)/MainForm.cs
+++ Administrator_company/Administrator_company/Preview (Test)/MainForm.cs
@@ -58,5 +58,41 @@ namespace Administrator_company.Preview__Test_
             StockForm stockForm = new StockForm();
             stockForm.Show();
         }
+
+        private void информацияОСотрудникахToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            InfoForm infoForm = new InfoForm();
+            infoForm.Show();
+        }
+
+        private void должностиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PositionForm positionForm = new PositionForm();
+            positionForm.Show();
+        }
+
+        private void сотрудникиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            EmployeesForm employeesForm = new EmployeesForm();
+            employeesForm.Show();
+        }
+
+        private void продуктыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ProductsForm productsForm = new ProductsForm();
+            productsForm.Show();
+        }
+
+        private void складToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StockForm stockForm = new StockForm();
+            stockForm.Show();
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutProgram aboutProgram = new AboutProgram();
+            aboutProgram.Show();
+        }
     }
 }
