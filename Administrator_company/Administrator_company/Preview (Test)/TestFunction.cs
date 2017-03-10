600fb35c2fe8a050367f864987ec7271accf3fa4
andreyks95 <andreyks95@gmail.com>
2017-03-10 14:11:17 +0200

    refactoring

--- Administrator_company/Administrator_company/Preview (Test)/TestFunction.cs
+++ Administrator_company/Administrator_company/Preview (Test)/TestFunction.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows.Forms;
-using Administrator_supermarket;
-
-namespace Administrator_company.Preview__Test_
+﻿namespace Administrator_company
 {
     public class TestFunction
     {
