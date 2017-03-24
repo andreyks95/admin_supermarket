b3bd51219753a05f3565d2ed706774ed9d58b358
andreyks95 <andreyks95@gmail.com>
2017-03-24 10:54:31 +0200

    Add button -> Report

--- Administrator_company/Administrator_company/Preview (Test)/EmployeesTestForm.Designer.cs
+++ Administrator_company/Administrator_company/Preview (Test)/EmployeesTestForm.Designer.cs
@@ -56,6 +56,7 @@
             this.textBoxSearch = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ReportButton = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -63,6 +64,7 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.ReportButton);
             this.panel1.Controls.Add(this.dateTimePicker2);
             this.panel1.Controls.Add(this.dateTimePicker1);
             this.panel1.Controls.Add(this.label9);
@@ -337,6 +339,16 @@
             this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
             this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
             // 
+            // ReportButton
+            // 
+            this.ReportButton.Location = new System.Drawing.Point(255, 40);
+            this.ReportButton.Name = "ReportButton";
+            this.ReportButton.Size = new System.Drawing.Size(75, 23);
+            this.ReportButton.TabIndex = 58;
+            this.ReportButton.Text = "Report";
+            this.ReportButton.UseVisualStyleBackColor = true;
+            this.ReportButton.Click += new System.EventHandler(this.ReportButton_Click);
+            // 
             // EmployeesTestForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -387,5 +399,6 @@
         private System.Windows.Forms.Button FirstRecordButton;
         private System.Windows.Forms.TextBox textBoxSearch;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button ReportButton;
     }
 }
